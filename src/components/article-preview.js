2b38c81f90732ec333f8ec0d334b52dbccd8a8a5
huitian <tanhuitian@gmail.com>
2019-04-29 20:06:48 +0800

    Update article preview to link picture

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -6,10 +6,10 @@ import styles from './article-preview.module.css'
 
 export default ({ article }) => (
   <div className={styles.preview}>
-    <Img alt="" fluid={article.heroImage.fluid} />
-    <h3 className={styles.previewTitle}>
-      <Link to={`/blog/${article.slug}`}>{article.title}</Link>
-    </h3>
+    <Link to={`/blog/${article.slug}`}>
+      <Img alt="" fluid={article.heroImage.fluid} />
+      <h4 className={styles.previewTitle}>{article.title}</h4>
+    </Link>
     <small>{article.publishDate}</small>
     <p
       dangerouslySetInnerHTML={{
