543221ffa33830457019831e7fe065614cc7cc98
huitian <tanhuitian@gmail.com>
2019-04-29 20:07:42 +0800

    Change Hero module to exclude image

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,9 +5,8 @@ import styles from './hero.module.css'
 
 export default ({ data }) => (
   <div className={styles.hero}>
-    <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} />
     <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
+      <h4 className={styles.heroHeadline}>{data.name}</h4>
       <p className={styles.heroTitle}>{data.title}</p>
       <p>{data.shortBio.shortBio}</p>
     </div>
