f5f5c4fbf0d82295a933fbe061254a3611eaf6a0
huitian <tanhuitian@gmail.com>
2019-04-30 11:58:31 +0800

    add about section

---
+++ src/components/about.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import {
+  Card,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  CardBody,
+} from 'reactstrap'
+
+const About = props => {
+  return (
+    <CardDeck className="container" id="about">
+      <Card className="card20" body outline color="grey">
+        <img
+          src="https://i.imgur.com/dVoFpzQ.png"
+          alt="Design"
+          className="skills"
+        />
+        <CardBody>
+          <CardTitle>
+            <h3>Design</h3>
+          </CardTitle>
+          <CardText />
+          <CardSubtitle>
+            <h5>TOOLS</h5>
+          </CardSubtitle>
+          <CardText>
+            Figma
+            <br />
+            Invision
+            <br />
+            Photoshop
+            <br />
+            Illustrator
+            <br />
+            Adode XD
+            <br />
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card className="card20" body outline color="grey">
+        <img
+          src="https://i.imgur.com/Q23f1p2.png"
+          alt="Development"
+          className="skills"
+        />
+        <CardBody>
+          <CardTitle>
+            <h3>Development</h3>
+          </CardTitle>
+          <CardSubtitle>
+            <h5>TOOLS</h5>
+          </CardSubtitle>
+          <CardText>
+            JavaScript (ES6)
+            <br />
+            HTML + CSS
+            <br />
+            React
+            <br />
+            React Native
+            <br />
+            MongoDB (No SQL)
+            <br />
+            with Mongoose
+            <br />
+            PostgresDB (Relational DB)
+            <br /> Sequelize
+            <br />
+            Express
+            <br />
+            Node.js
+            <br />
+            Jest
+            <br />
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card className="card20" body outline color="grey">
+        <img
+          src="https://i.imgur.com/RR4aICr.png"
+          alt="Others"
+          className="skills"
+        />
+        <CardBody>
+          <CardTitle>
+            <h3>Other Skills</h3>
+          </CardTitle>
+          <CardSubtitle>
+            <h5>SKILLS</h5>
+          </CardSubtitle>
+          <CardText>
+            Test-driven development
+            <br />
+            Agile Methodology
+            <br />
+            React, React Native
+            <br />
+            Pair programming <br />
+            Express
+            <br />
+            Node
+            <br />
+          </CardText>
+        </CardBody>
+      </Card>
+    </CardDeck>
+  )
+}
+
+export default About
