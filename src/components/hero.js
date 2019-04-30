bf9f5a50c800d89cba63de70e8eb46c04635062f
huitian <tanhuitian@gmail.com>
2019-04-30 12:06:43 +0800

    Edit hero image styling

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import Img from 'gatsby-image'
-
 import styles from './hero.module.css'
 
 export default ({ data }) => (
   <div className={styles.hero}>
     <div className={styles.heroDetails}>
-      <h4 className={styles.heroHeadline}>{data.name}</h4>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
+      <p className={styles.heroTitle}>Hi, I am </p>
+      <h1 className={styles.heroHeadline}>{data.name},</h1>
+      <p className={styles.heroTitle}>{data.title}.</p>
+      {/* <p>{data.shortBio.shortBio}</p> */}
     </div>
   </div>
 )
