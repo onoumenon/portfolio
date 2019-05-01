c928a7e94f9a3ed9defe31c3b566e9f986bd220f
huitian <tanhuitian@gmail.com>
2019-05-01 17:12:34 +0800

    remove margin top for p

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -11,11 +11,11 @@ export default ({ article }) => (
       <h4 className={styles.previewTitle}>{article.title}</h4>
     </Link>
     <small>{article.publishDate}</small>
-    {/* <p
+    <p
       dangerouslySetInnerHTML={{
         __html: article.description.childMarkdownRemark.html,
       }}
-    /> */}
+    />
     {article.tags.map(tag => (
       <p className={styles.tag} key={tag}>
         {tag}
