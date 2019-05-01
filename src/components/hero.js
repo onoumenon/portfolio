23016111593912089d8ec7da4bb6c20d567a1746
huitian <tanhuitian@gmail.com>
2019-05-01 22:03:55 +0800

    Refine typography in hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './hero.module.css'
 
 export default ({ data }) => (
-  <div className={styles.hero}>
-    <div className={styles.heroDetails}>
-      <p className={styles.heroTitle}>Hi, I am </p>
-      <h1 className={styles.heroHeadline}>{data.name},</h1>
-      <p className={styles.heroTitle}>a {data.title}.</p>
-      {/* <p>{data.shortBio.shortBio}</p> */}
+  <>
+    <div className={styles.hero}>
+      <div className={styles.heroDetails}>
+        <h3 className={styles.heroTitle}>Hi, I am </h3>
+        <h1 className={styles.heroHeadline}>Hui Tian,</h1>
+        <h3 className={styles.heroTitle}>a {data.title}.</h3>
+        {/* <p>{data.shortBio.shortBio}</p> */}
+      </div>
     </div>
-  </div>
+  </>
 )
