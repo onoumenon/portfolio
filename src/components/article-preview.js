3049196d118719978fdd9736eacab2fa073b4deb
huitian <tanhuitian@gmail.com>
2019-05-05 22:21:18 +0800

    Use Card component for article preview

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  CardBody,
+} from 'reactstrap'
 import Img from 'gatsby-image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './article-preview.module.css'
 
 export default ({ article }) => (
-  <div className={styles.preview}>
-    <Link to={`/blog/${article.slug}`}>
-      <Img alt="" fluid={article.heroImage.fluid} />
-      <h4 className={styles.previewTitle}>{article.title}</h4>
-    </Link>
-    <small>{article.publishDate}</small>
-    <br />
-    <a href={article.url}>
-      <FontAwesomeIcon className="icon" icon="globe-asia" />
-    </a>
-    <a href={article.github}>
-      <FontAwesomeIcon className="icon" icon={['fab', 'github']} />
-    </a>
-    <br />
-    {article.tags.map(tag => (
-      <p className={styles.tag} key={tag}>
-        {tag}
-      </p>
-    ))}
-  </div>
+  <Card>
+    <CardBody>
+      <Link to={`/blog/${article.slug}`}>
+        <CardImg alt={article.slug} src={article.heroImage.fluid.src} />
+        <CardTitle className={styles.previewTitle}>{article.title}</CardTitle>
+      </Link>
+
+      <a href={article.url}>
+        <FontAwesomeIcon className="icon" icon="globe-asia" />
+      </a>
+      <a href={article.github}>
+        <FontAwesomeIcon className="icon" icon={['fab', 'github']} />
+      </a>
+      <br />
+      {article.tags.map(tag => (
+        <p className={styles.tag} key={tag}>
+          {tag}
+        </p>
+      ))}
+    </CardBody>
+  </Card>
 )
