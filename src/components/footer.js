9c9912ac4429e4dd227bbff304ff8c60e0f7afb4
huitian <tanhuitian@gmail.com>
2019-05-06 09:55:39 +0800

    remove * css

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ function Footer() {
     <footer className="pt-4">
       <div className="text-center py-3">
         <small>
-          Made with Gatsby and Contentful © 2019 Tan Hui Tian, toolbox illustrations by{' '}
-          <a href="https://icons8.com">Ouch.pics</a>
+          Made with Gatsby and Contentful © 2019 Tan Hui Tian, toolbox
+          illustrations by <a href="https://icons8.com">Ouch.pics</a>
         </small>
       </div>
     </footer>
